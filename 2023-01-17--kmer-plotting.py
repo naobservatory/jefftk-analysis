c5bd44fdbbc2b91c3c40003281438c4cc8d892a2
Jeff Kaufman <jeff@jefftk.com>
2023-01-18 15:06:46 -0500

    switch to working with totals until the very end to fix banding issues

--- 2023-01-17--kmer-plotting.py
+++ 2023-01-17--kmer-plotting.py
@@ -9,7 +9,13 @@ import numpy as np
 
 wtp, fname_in, fname_out, xmax, ymax = sys.argv[1:]
 
-xmax = int(xmax)
+samples = 0
+with open("longest-timeseries.tsv") as inf:
+    for line in inf:
+        if line.strip().split("\t")[-1] == wtp:
+            samples += 1
+
+xmax = int(xmax) * samples
 ymax = int(ymax)
 
 min_x = max_x = min_y = max_y = None
@@ -19,29 +25,29 @@ all_y = set()
 
 with open(fname_in) as inf:
     for line in inf:
-        _, mean, variance = line.strip().split()
-        mean = float(mean)
+        _, total, variance = line.strip().split()
+        total = float(total)
         variance = float(variance)
-        if min_x is None or mean < min_x:
-            min_x = mean
-        if max_x is None or mean > max_x:
-            max_x = mean
+        if min_x is None or total < min_x:
+            min_x = total
+        if max_x is None or total > max_x:
+            max_x = total
         if min_y is None or variance < min_y:
             min_y = variance
         if max_y is None or variance > max_y:
             max_y = variance
 
-        all_x.add(mean)
+        all_x.add(total)
         all_y.add(variance)
 
 min_x = min_y = 0
-        
+
 all_x = list(sorted(all_x))
 all_y = list(sorted(all_y))
 
 incr_x = min((a-b) for (a,b) in zip(all_x[1:], all_x))
 incr_y = min((a-b) for (a,b) in zip(all_y[1:], all_y))
-            
+
 if max_x > xmax:
     max_x = xmax
 if max_y > ymax:
@@ -65,7 +71,7 @@ def to_ypip(val):
     return int((val - min_y)*Y_SCALAR)
 
 def xpip_to_real(xpip):
-    return (xpip + min_x*X_SCALAR) / X_SCALAR
+    return (xpip + min_x*X_SCALAR) / X_SCALAR / samples
 
 def ypip_to_real(ypip):
     return (ypip + min_y*Y_SCALAR) / Y_SCALAR
@@ -78,24 +84,24 @@ for y_val in range(ypips):
     data.append(row)
 
 print(len(data[0]), len(data))
-    
+
 with open(fname_in) as inf:
     for line in inf:
-        count, mean, variance = line.strip().split()
+        count, total, variance = line.strip().split()
         count = int(count)
-        mean = float(mean)
+        total = float(total)
         variance = float(variance)
 
-        xpip = to_xpip(mean)
+        xpip = to_xpip(total)
         ypip = to_ypip(variance)
-        
+
         if xpip >= xpips or ypip >= ypips:
             continue
-        
+
         try:
             data[ypip][xpip] += count
         except IndexError:
-            print(mean, to_xpip(mean), min_x,max_x)
+            print(total, to_xpip(total), min_x,max_x)
             print(variance, to_ypip(variance), min_y,max_y)
             print(len(data))
             print(len(data[0]))
@@ -115,8 +121,8 @@ y = [ypip_to_real(ypip) for ypip in range(len(data))]
 fig, ax = plt.subplots()
 ax.pcolormesh(x, y, data)
 ax.set_ylabel("variance")
-ax.set_xlabel("mean abundance")
-ax.set_title("%s per-kmer normalized variance as a function of abundance" % wtp)
-ax.set_xlim(xmax=xmax)
+ax.set_xlabel("total abundance")
+ax.set_title("%s per-kmer variance as a function of abundance" % wtp)
+ax.set_xlim(xmax=xmax/samples)
 ax.set_ylim(ymax=ymax)
 fig.savefig(fname_out, dpi=180)
