053bdd18726266c6b3d68fab5bfd75c1cfb4968e
Jeff Kaufman <jeff@jefftk.com>
2023-01-30 15:57:51 -0500

    plot it all with one command

--- 2023-01-27--plot-accuracy-by-location.py
+++ 2023-01-27--plot-accuracy-by-location.py
@@ -3,40 +3,43 @@
 import sys
 import glob
 
-slug, = sys.argv[1:]
+slugs = sys.argv[1:]
+viruses = {}
+for slug in slugs:
+    d = {
+        "assembled": {},
+        "genbank_top": {},
+        "genbank_closest": {},
+    }
 
-d = {
-    "assembled": {},
-    "genbank_top": {},
-    "genbank_closest": {},
-}
+    d["assembled"]["fasta"], = glob.glob("%s.*.assembled.fasta" % slug)
+    d["genbank_top"]["fasta"] = d["assembled"]["fasta"].replace(
+        ".assembled.", ".")
+    d["genbank_closest"]["fasta"], = (
+        set(glob.glob("%s.*.fasta" % slug)) -
+        set([d["assembled"]["fasta"], d["genbank_top"]["fasta"]]))
 
-d["assembled"]["fasta"], = glob.glob("%s.*.assembled.fasta" % slug)
-d["genbank_top"]["fasta"] = d["assembled"]["fasta"].replace(".assembled.", ".")
-d["genbank_closest"]["fasta"], = (
-    set(glob.glob("%s.*.fasta" % slug)) -
-    set([d["assembled"]["fasta"], d["genbank_top"]["fasta"]]))
+    for k in d.values():
+        k["loc_full"] = []
+        k["loc_any"] = []
+        k["loc_rc"] = []
 
-for k in d.values():
-    k["loc_full"] = []
-    k["loc_any"] = []
-    k["loc_rc"] = []
+        for fname in glob.glob(
+                "reads-%s/*.locs.tsv" % k["fasta"].replace(".fasta", "")):
+            with open(fname) as inf:
+                for line in inf:
+                    loc, obs_full, obs_any, obs_rc = [
+                        int(x) for x in line.strip().split()]
 
-    for fname in glob.glob(
-            "reads-%s/*.locs.tsv" % k["fasta"].replace(".fasta", "")):
-        with open(fname) as inf:
-            for line in inf:
-                loc, obs_full, obs_any, obs_rc = [
-                    int(x) for x in line.strip().split()]
+                    if loc >= len(k["loc_full"]):
+                        k["loc_full"].append(0)
+                        k["loc_any"].append(0)
+                        k["loc_rc"].append(0)
 
-                if loc >= len(k["loc_full"]):
-                    k["loc_full"].append(0)
-                    k["loc_any"].append(0)
-                    k["loc_rc"].append(0)
-
-                k["loc_full"][loc] += obs_full
-                k["loc_any"][loc] += obs_any
-                k["loc_rc"][loc] += obs_rc
+                    k["loc_full"][loc] += obs_full
+                    k["loc_any"][loc] += obs_any
+                    k["loc_rc"][loc] += obs_rc
+    viruses[slug] = d
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
@@ -46,39 +49,63 @@ labels = [
     ("loc_any", "any k-mer matches"),
     ("loc_rc", "whole reads where any k-mer matches"),
 ]
-    
 
-for c, k in d.items():
-    plt.clf()
-    fig, ax = plt.subplots()
+for slug, d in viruses.items():
+    for c, k in d.items():
+        plt.close()
+        plt.clf()
+        fig, ax = plt.subplots()
 
-    ax.set_ylabel("reads")
-    ax.set_xlabel("position along genome")
-    ax.set_title("%s: reads by location (%s)" % (slug.upper(), c))
-    loc_percentages = [x / len(k["loc_full"]) * 100
-                       for x in range(len(k["loc_full"]))]
+        ax.set_ylabel("reads")
+        ax.set_xlabel("position along genome")
+        ax.set_title("%s: reads by location (%s)" % (slug.upper(), c))
+        loc_percentages = [x / len(k["loc_full"]) * 100
+                           for x in range(len(k["loc_full"]))]
+
+        for tag, label in labels:
+            ax.plot(loc_percentages, k[tag], label=label)
+
+        ax.xaxis.set_major_formatter(mtick.PercentFormatter())
+        ax.legend()
+        fig.savefig("%s-%s-matches.png" % (slug, c), dpi=180)
 
     for tag, label in labels:
-        ax.plot(loc_percentages, k[tag], label=label)
+        plt.close()
+        plt.clf()
+        fig, ax = plt.subplots()
 
-    ax.xaxis.set_major_formatter(mtick.PercentFormatter())
-    ax.legend()
-    fig.savefig("%s-%s-matches.png" % (slug, c), dpi=180)
+        ax.set_ylabel("reads")
+        ax.set_xlabel("position along genome")
+        ax.set_title("%s: reads by location (%s)" % (slug.upper(), label))
 
-for tag, label in labels:
-    plt.clf()
-    fig, ax = plt.subplots()
+        for c, k in d.items():
+            loc_percentages = [x / len(k[tag]) * 100
+                               for x in range(len(k[tag]))]
+            ax.plot(loc_percentages, k[tag], label=c)
 
-    ax.set_ylabel("reads")
-    ax.set_xlabel("position along genome")
-    ax.set_title("%s: reads by location (%s)" % (slug.upper(), label))
+        ax.xaxis.set_major_formatter(mtick.PercentFormatter())
+        ax.legend()
+        fig.savefig("%s-%s-matches.png" % (slug, tag), dpi=180)
+        
 
-    for c, k in d.items():
-        loc_percentages = [x / len(k[tag]) * 100
-                           for x in range(len(k[tag]))]
-        ax.plot(loc_percentages, k[tag], label=c)
+plt.close()
+plt.clf()
+tag="loc_full"
+tagname="whole reads where any k-mer matches"
+fig, ax = plt.subplots(nrows=len(viruses), sharex=True, figsize=(6,12),
+                       constrained_layout=True)
+#fig.tight_layout()
+plt.suptitle("reads by location (%s)" % tagname)
+fig.supxlabel("scaled reads")
+fig.supylabel("position along genome")
+for i, (slug, d) in enumerate(sorted(viruses.items())):
+    k = d["assembled"]
+    loc_percentages = [x / len(k[tag]) * 100
+                       for x in range(len(k[tag]))]
 
-    ax.xaxis.set_major_formatter(mtick.PercentFormatter())
-    ax.legend()
-    fig.savefig("%s-%s-matches.png" % (slug, tag), dpi=180)
-    
+    #ax[i].grid(visible=True, axis='y')
+    ax[i].plot(loc_percentages, k[tag], label=slug)
+    ax[i].xaxis.set_major_formatter(mtick.PercentFormatter())
+    ax[i].ticklabel_format(axis='y', style='sci')
+    ax[i].set_title(slug)
+    fig.savefig("all-matches.png", dpi=180)
