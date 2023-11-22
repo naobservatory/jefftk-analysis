6e2f1fde35f2a2d88557f9839b95212604035894
Jeff Kaufman <jeff@securebio.org>
2023-11-22 21:18:44 +0000

    check in work

--- 2023-11-22--bowtie-on-kraken-unclassified.py
+++ 2023-11-22--bowtie-on-kraken-unclassified.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import glob
+import gzip
 import json
 import math
 import random
@@ -59,20 +61,13 @@ Alignment = namedtuple(
 def process_sample(paper, bioproject, sample):
     dashboard_dir = bioproject_to_dashboard_dir[bioproject]
 
-    hvr = {}
-    hvr_fname = os.path.join(
-        dashboard_dir, "hvreads", "%s.hvreads.json" % sample)
-    if os.path.exists(hvr_fname):
-        with open(hvr_fname) as inf:
-            hvr = json.load(inf)
-
-    hvr = {}
-    hvr_fname = os.path.join(
-        dashboard_dir, "hvreads", "%s.hvreads.json" % sample)
-    if os.path.exists(hvr_fname):
-        with open(hvr_fname) as inf:
-            hvr = json.load(inf)
-
+    kraken_unclassified_read_ids = set()
+    for kraken_output in glob.glob("*.kraken2.tsv.gz"):
+        with gzip.open(kraken_output, "rt") as inf:
+            for line in inf:
+                if line.startswith("U"):
+                    kraken_unclassified_read_ids.add(line.split("\t")[1])
+    
     als = defaultdict(list)
     als_fname = os.path.join("%s.alignments.masked-41.tsv" % sample)
     if os.path.exists(als_fname):
@@ -89,19 +84,14 @@ def process_sample(paper, bioproject, sample):
                     int(score),
                     int(length)))
 
-    n_hit_hv = 0
-    n_aligned_hv = 0
-    n_hit_but_not_aligned = 0
-    n_aligned_but_not_hit = 0
-
     printed = 0
 
     # read_id -> info
     to_print = defaultdict(list)
     
     for read_id in sorted(als, key=lambda k: hashlib.sha256(k.encode('utf-8')).digest()):
-        #assigned_taxid = hvr[read_id][0]
-        #kraken_output = hvr[read_id][1]
+        if read_id not in kraken_unclassified_read_ids:
+            continue
 
         combined_score = sum(al.score for al in als[read_id])
         combined_length = sum(al.length for al in als[read_id])
@@ -113,19 +103,18 @@ def process_sample(paper, bioproject, sample):
 
         aligned_taxid, = set(al.taxid for al in als[read_id])
         
-        if read_id not in hvr:
-            to_print[read_id].append(read_id)
-            to_print[read_id].append("bowtie: %.0f" % normalized_score)
-            to_print[read_id].append("%s %s" % (
-                aligned_taxid, human_viruses.get(aligned_taxid, "absent")))
-            for al in als[read_id]:
-                to_print[read_id].append(
-                    "  %s %s %s %s" % (
-                        al.taxid, al.cigar, al.genome, al.ref_start))
-            printed += 1
+        to_print[read_id].append(read_id)
+        to_print[read_id].append("bowtie: %.0f" % normalized_score)
+        to_print[read_id].append("%s %s" % (
+            aligned_taxid, human_viruses.get(aligned_taxid, "absent")))
+        for al in als[read_id]:
+            to_print[read_id].append(
+                "  %s %s %s %s" % (
+                    al.taxid, al.cigar, al.genome, al.ref_start))
+        printed += 1
 
-            if printed == 10:
-                break
+        if printed == 20:
+            break
 
     for suffix in [".pair1.truncated", ".pair2.truncated", ".collapsed"]:
         with open("%s%s" % (sample, suffix)) as inf:
@@ -133,7 +122,6 @@ def process_sample(paper, bioproject, sample):
                 read_id = title.split()[0]
                 if read_id in to_print:
                     to_print[read_id].append(sequence)
-
 
     for read_id, lines in sorted(to_print.items()):
         for line in lines:
