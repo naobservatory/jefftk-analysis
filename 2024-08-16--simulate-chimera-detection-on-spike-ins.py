f5842af2f4ee2164332c85c6fd9e9e85d074800a
Jeff Kaufman <jeff@jefftk.com>
2024-08-16 14:51:15 -0400

    convert to poisson

--- 2024-08-16--simulate-chimera-detection-on-spike-ins.py
+++ 2024-08-16--simulate-chimera-detection-on-spike-ins.py
@@ -6,11 +6,6 @@ from statistics import geometric_mean
 
 rng = np.random.default_rng()
 
-# redo all of this with
-#  - poisson
-#  - relative abundances
-#  - target concentrations
-
 # concentration -> number of flagged reads at each of the four junctions
 flags =  {
     "710/mL": [9, 1, 5, 7],
@@ -18,68 +13,33 @@ flags =  {
     "71000/mL": [1097, 238, 508, 700],
 }
 
-def simulate_one(concentration, subsampling):
-    real_flags = flags[concentration]
-    n_real = sum(flags[concentration])
-    n_simulated = round(n_real * subsampling)
+depths = {
+    "710/mL": 2.51e8,
+    "7100/mL": 1.1e9,
+    "71000/mL": 2.39e8,
+}
 
-    if n_simulated < 1:
-        return False
+ra_flags = {}
+for concentration in flags:
+    ra_flags[concentration] = [
+        x/depths[concentration] for x in flags[concentration]]
 
-    probabilities = [n / n_real for n in real_flags]
+def simulate_one(concentration, read_depth):
+    for ra in ra_flags[concentration]:
+        abundance = rng.poisson(ra * read_depth)
+        if abundance >= 2:
+            return True
+    return False
 
-    observations = Counter()
-    for observation in rng.choice(len(real_flags), size=n_simulated, replace=True,
-                                  p=probabilities):
-        observations[observation] += 1
+n_simulations_per_scenario = 100000
 
-    (junction, count), = observations.most_common(1)
-
-    return count > 1
-
-
-n_simulations_per_scenario = 10000
-
-subsamples = [
-    0.237,
-    0.154,
-    0.1,
-    0.0750,
-    0.0562,
-    0.0421,
-    0.0316,
-    0.0237,
-    0.0154,
-    0.01,
-    0.00750,
-    0.00562,
-]
-
-for s1, s2 in zip(subsamples, subsamples[1:]):
-    subsamples.append(round(geometric_mean([s1, s2]), ndigits=5))
-subsamples.sort(reverse=True)
-
-for s1, s2 in zip(subsamples, subsamples[1:]):
-    subsamples.append(round(geometric_mean([s1, s2]), ndigits=5))
-subsamples.sort(reverse=True)
-
-for s1, s2 in zip(subsamples, subsamples[1:]):
-    subsamples.append(round(geometric_mean([s1, s2]), ndigits=5))
-subsamples.sort(reverse=True)
-
-
-
-
-print("subsample", *sorted(flags), sep="\t")
-for subsample in subsamples:
-    row = [subsample]
-    # get all the concentrations into the same range for a faster simuation
-    effective_subsample = subsample
+print("Read Depth", *sorted(flags), sep="\t")
+for read_depth in np.logspace(np.log10(1e9), np.log10(1e3), 100):
+    row = [int(read_depth)]
     for concentration in sorted(flags):
         t = 0
         for _ in range(n_simulations_per_scenario):
-            if simulate_one(concentration, effective_subsample):
+            if simulate_one(concentration, read_depth):
                 t += 1
         row.append(t / n_simulations_per_scenario)
-        effective_subsample = subsample / 10
     print(*row, sep="\t")
